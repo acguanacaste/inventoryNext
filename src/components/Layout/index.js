1ce1068f38254eba28c9f5ce71136fc5e7100a2c
Federico Matarrita Arata <federico@usematics.com>
2021-08-18 18:25:00 -0600

    Some theming

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,24 @@
-import {Container} from "@material-ui/core";
+import {Container, Grid, Box} from "@material-ui/core";
 import TopMenu from "../TopMenu";
+import LanguageSwitch from "../LanguageSwitch";
+
 const Layout = ({children}) => {
     return (
         <Container maxWidth={"xl"} >
-            <TopMenu />
-            {children}
+            <Grid container spacing={2}>
+                <Grid item direction="row" container>
+                    <Grid item xs={6}>
+                        <TopMenu />
+                    </Grid>
+                    <Grid item xs alignSelf={"flex-end"}>
+                        <LanguageSwitch />
+                    </Grid>
+                </Grid>
+                <Grid item container>
+                    {children}
+                </Grid>
+            </Grid>
         </Container>
-        
     )
 }
 

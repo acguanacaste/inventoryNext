5989b1bdea4eea78bfb36371e68c958353aace4a
Federico Matarrita Arata <federico@usematics.com>
2021-09-02 12:00:56 -0600

    Clean up and formatting

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NextLink from 'next/link';
+import NextLink from "next/link";
 import {makeStyles} from "@material-ui/core";
 import theme from "../../theme/theme";
 import {useRouter} from "next/router";
@@ -8,20 +8,25 @@ const useStyles = makeStyles((theme) => ({
     link : {
         color:theme.palette.text.link,
         '&:hover': {color:theme.palette.text.hover},
-        '&:visited': {color:theme.palette.text.link}
+        '&:visited': {color:theme.palette.text.link},
+        textDecoration:'none',
     }
 }))
 
 const Link = ({href, target, children, ...props}) =>{
     
     const classes = useStyles(theme);
-    const t = target?target:"_self";
-    const {locale} = useRouter()
-    const l =props.locale?props.locale:locale
-    
+    const t = target?target:'_self';
+    const {locale} = useRouter();
+    const l =props.locale?props.locale:locale;
+   
     return(
         <NextLink href={href} locale={l} >
-            <a {...props} className={classes.link} target={t}>
+            <a
+                {...props}
+                className={classes.link}
+                target={t}
+            >
                 {children}
             </a>
         </NextLink>
